a5d88948778cc52d920379296f3e91e675c3112a
dreamer <2761564455@qq.con>
2019-03-01 22:55:24 +0800

    2.28

--- code/fix_bug.py
+++ code/fix_bug.py
@@ -1,11 +1,15 @@
 # import cv2
-# import numpy as np
+import numpy as np
 #
 # img = np.zeros((100, 100), dtype=np.uint8)
 # cv2.imshow('initial', img)
 #
 # cv2.waitKey()
 # cv2.destroyAllWindows()
-a = 10
-b = '{:08b}'.format(a)
-print(b)
+# a = 10
+# b = '{:08b}'.format(a)
+# print(b)
+# a = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+# b = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+# c =a+b
+# print(c)
