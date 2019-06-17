8f627380bca93052f3547fe838969c0c38f6cb14
dreamer <2761564455@qq.con>
2019-06-17 21:30:37 +0800

    实现surf算法

--- 特征检测算法/surf.py
+++ 特征检测算法/surf.py
@@ -4,16 +4,26 @@ import numpy as np
 
 imgpath = sys.argv[1]
 img = cv2.imread(imgpath)
+alg = sys.argv[2] #指定使用的是哪一个特征检测算法
+
+def fd(algorithm):
+    if algorithm == 'SIFT':
+        return cv2.xfeatures2d.SIFT_create() #实例化sift对象
+    else:
+        return cv2.xfeatures2d.SURF_create(float(sys.argv[3]) if len(sys.argv) == 4 else 4000)
+
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 sift = cv2.xfeatures2d.SIFT_create() #实例化一个对象。
-keypoints,descriptor = sift.detectAndCompute(gray,None) #进行检测和计算sift特征点
-#每一个特征点的描述子为一个128维的向量
+keypoints,descriptor = sift.detectAndCompute(gray,None) #进行检测和计算特征点描述子
+
 
 img = cv2.drawKeypoints(image = img,outImage=img,keypoints=keypoints,flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
                         color=(51,163,236))
+
 cv2.imwrite("sift_keypoints.jpg",img)
 cv2.imshow('sift_keypoints',img)
+
 while(True):
     if cv2.waitKey(int(1000 / 12)) & 0xff == ord("q"):
         break
